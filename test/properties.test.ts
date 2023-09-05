6be224a1f610fd855ee1d7a8a2de0c8c15d44356
Gustio Nusamba <gustio.n@students.amikom.ac.id>
2023-09-05 21:17:16 +0700

    Properties in class

---
+++ test/properties.test.ts
@@ -0,0 +1,23 @@
+describe('Properties', () => {
+    class Customer {
+        readonly id: number;
+        name: string;
+        address?: string;
+
+        constructor(id: number, name: string) {
+            this.id = id;
+            this.name = name;
+        }
+    }
+
+    it('should support properties in the class', () => {
+        const customer = new Customer(1, "John");
+        expect(customer.id).toBe(1);
+        expect(customer.name).toBe("John");
+        expect(customer.address).toBeUndefined();
+
+        console.info("Customer id: " + customer.id);
+        console.info("Customer name: " + customer.name);
+        console.info("Customer address: " + customer.address);
+    });
+});
