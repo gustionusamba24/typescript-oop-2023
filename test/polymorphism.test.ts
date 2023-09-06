aeca159673b651b29b0c63d27dc83bbb268f1823
Gustio Nusamba <gustio.n@students.amikom.ac.id>
2023-09-06 14:53:54 +0700

    Type Cast

--- test/polymorphism.test.ts
+++ test/polymorphism.test.ts
@@ -4,11 +4,22 @@ describe('Polymorphism', () => {
         }
     }
 
-    class Manager extends Employee {}
-    class VicePresident extends Employee {}
+    class Manager extends Employee {
+    }
+
+    class VicePresident extends Employee {
+    }
 
     function sayHello(employee: Employee): void {
-        console.info(`Hello ${employee.name}`);
+        if (employee instanceof VicePresident) {
+            const vp = employee as VicePresident;
+            console.info(`Hello VP ${vp.name}`)
+        } else if (employee instanceof Manager) {
+            const manager = employee as Manager;
+            console.info(`Hello manager ${manager.name}`)
+        } else {
+            console.info(`Hello employee ${employee.name}`)
+        }
     }
 
     it('should support polymorphism', () => {
